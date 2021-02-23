c2aa09dfcadbfdaedafb1123041d65d31d9ca6f2
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-02-23 14:06:03 -0800

    Set up folder structure, with redux implementation in mind.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+//redux set up
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
 
 ReactDOM.render(
+  // <Provider store={store}>
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  // </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
